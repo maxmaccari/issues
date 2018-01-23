e682ec81ac06b2415252d2f80a77ec82a8d0e0ef
Maxsuel Maccari <maxmaccari@hotmail.com>
2018-01-23 20:19:38 -0300

    Configure docs generation

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Issues.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      name: "Issues",
+      source_url: "https://www.github.com/maxmaccari/issues",
       deps: deps()
     ]
   end
@@ -23,7 +25,9 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       { :httpoison, "~> 0.9" },
-      { :poison, "~> 2.2" }
+      { :poison, "~> 2.2" },
+      { :ex_doc, "~> 0.12" },
+      { :earmark, "~> 1.0", override: true }
     ]
   end
 
